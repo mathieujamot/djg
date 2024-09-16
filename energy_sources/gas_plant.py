69ecda01e1ee6c3e4b9e3c0807dfb086ebed070a
Your Name <you@example.com>
2024-09-16 22:48:59 +0200

    removed energy sources

--- energy_sources/gas_plant.py
+++
@@ -1,8 +0,0 @@
-class GasPlant:
-    def __init__(self, capacity_mw, efficiency):
-        self.capacity_mw = capacity_mw
-        self.efficiency = efficiency
-
-    def production(self, hours):
-        """Calculate energy production in MWh for a given number of hours."""
-        return self.capacity_mw * self.efficiency * hours
--- energy_sources/hydro_plant.py
+++
@@ -1,9 +0,0 @@
-class HydroPlant:
-    def __init__(self, capacity_mw, water_flow_rate):
-        self.capacity_mw = capacity_mw
-        self.water_flow_rate = water_flow_rate
-
-    def production(self, hours):
-        """Calculate energy production in MWh for a given number of hours."""
-        # Simplified model: production is proportional to water flow rate
-        return self.capacity_mw * (self.water_flow_rate / 100) * hours
